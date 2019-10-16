2af78bb28cb5e929553e2744599741e45a607710
petermcclory <petermcclory@gmail.com>
2019-10-16 12:21:39 +0100

    Initial game mechanics and control complete

---
+++ Assets/Spawner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+
+    public float spawnRate = 1f;
+
+    public GameObject hexagonPrefab;
+
+    private float nextTimeToSpawn = 0f;
+
+    // Start is called before the first frame update
+    void Update()
+    {
+        if (Time.time >= nextTimeToSpawn)
+        {
+            Instantiate(hexagonPrefab, Vector3.zero, Quaternion.identity);
+            nextTimeToSpawn = Time.time + 1f / spawnRate;
+        }
+        
+    }
+}
